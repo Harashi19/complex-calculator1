da777fe4e90055911339d3dd216d2aa205430418
Lee <nastylee19@gmail.com>
2025-07-26 04:16:44 +0200

    replace if with switch

--- main.c
+++ main.c
@@ -6,12 +6,12 @@ int main() {
     int option;
 
     printf("1. Addition\n");
-printf("2. Subtraction\n");
-printf("3. Multiplication\n");
-printf("4. Division\n");
-printf("5. Modulus\n");
-printf("6. Equality Check\n");
-
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Modulus\n");
+    printf("6. Equality Check\n");
+    printf("7. Exit\n");
 
 
     printf("Enter operation: ");
@@ -27,41 +27,52 @@ printf("6. Equality Check\n");
         scanf("%lf %lf", &a.real, &a.imag);
     }
 
+    switch (option) {
+        case 1:
+            result = add(a, b);
+            printf("Addition = ");
+            print_complex(result);
+            break;
 
+        case 2:
+            result = subtract(a, b);
+            printf("Subtraction = ");
+            print_complex(result);
+            break;
 
-    if (option == 1) {
-        result = add(a, b);
-        printf("Sum = ");
-        print_complex(result);
-    } else if (option == 2) {
-        result = subtract(a, b);
-        printf("Difference = ");
-        print_complex(result);
-    } else if (option == 3) {
-        result = multiply(a, b);
-        printf("Product = ");
-        print_complex(result);
-    } else if (option == 4) {
-        result = divide(a, b);
-        printf("Quotient = ");
-        print_complex(result);
-    } else if (option == 5) {
-        double mod = modulus(a);
-        printf("Modulus = %.2f\n", mod);
-    }else if (option == 6) {
-        if (are_equal(a, b)) {
-            printf("The complex numbers are equal.\n");
-        } else {
-            printf("The complex numbers are not equal.\n");
-        }
-    }else {
-        printf("Invalid option!\n");
-        return 1;
+        case 3:
+            result = multiply(a, b);
+            printf("Multiplication = ");
+            print_complex(result);
+            break;
+
+        case 4:
+            result = divide(a, b);
+            printf("Division = ");
+            print_complex(result);
+            break;
+
+        case 5:
+            double mod = modulus(a);
+            printf("Modulus = %.2f\n", mod);
+            break;
+
+        case 6:
+            if (are_equal(a, b)) {
+                printf("The complex numbers are equal.\n");
+            }
+            else {
+                printf("The complex numbers are not equal.\n");
+            }
+            break;
+
+        case 7:
+            printf("goodbye!\n");
+            break;
+
+        default:
+            printf("Invalid option!\n");
+            return 1;
     }
-
-
-
-
-   // print_complex(result);
     return 0;
 }
